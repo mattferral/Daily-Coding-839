2beb74e0669f701fc4bec8bd39c2f058d28acf23
Matt <mattferral@gmail.com>
2024-08-04 23:08:42 -0400

    first commit

---
+++ solution.py
@@ -0,0 +1,11 @@
+def solution(mat, dict):
+    pass
+
+
+if __name__ == "__main__":
+    dict_1 = ['eat', 'rain', 'in', 'rat']
+    mat_1 = [['e', 'a', 't'], ['t', 't', 'i'], ['a', 'r', 'a']]
+    print(solution(mat_1))
+
+
+    assert solution(mat_1) == 3
